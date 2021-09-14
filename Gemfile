b85d192530fbead95d51ce96f52fdb90583d4647
TheSmileyDroid <ghsilha@gmail.com>
2021-09-13 21:33:48 -0300

    keep alive

--- Gemfile
+++ Gemfile
@@ -2,5 +2,12 @@ source 'https://rubygems.org' do
 	ruby '3.0.2'
     gem "pg"
     gem "discordrb"
+    gem "sinatra"
+    gem "thin"
+    gem "puma"
+    gem "reel"
+    gem "http"
+    gem "webrick"
     gem "solargraph"
 end
+
--- roby.rb
+++ roby.rb
@@ -1,10 +1,26 @@
 require 'discordrb'
 require 'pg'
+require 'sinatra'
 
-bot = Discordrb::Bot.new token: ENV['BOT_TOKEN']
 
-bot.message(with_text: 'Ping!') do |event|
+
+#conn = PG::Connection.open(:dbname => 'test')
+
+@bot = Discordrb::Bot.new token: ENV['BOT_TOKEN']
+
+
+@bot.message(with_text: 'Ping!') do |event|
   event.respond 'Pong!'
 end
 
-bot.run
+@bot.message(with_text: 'Count') do |event|
+  event.respond 'Contagem de "Pongs!": '
+end
+
+
+get '/' do
+  "I'm alive!"
+end
+t = Thread.new() {
+  @bot.run
+}
