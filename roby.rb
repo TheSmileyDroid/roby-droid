a1c8a7ef749a638606b2b9244f3e017775b54e7a
TheSmileyDroid <ghsilha@gmail.com>
2021-09-13 21:05:42 -0300

    ENV

--- roby.rb
+++ roby.rb
@@ -1,7 +1,7 @@
 require 'discordrb'
 require 'pg'
 
-bot = Discordrb::Bot.new token: 'ODg3MDc1NjgxMjQ5NDA2OTg2.YT-3mw.bwASEeUWeAagOuf4PrWNR5G0snk'
+bot = Discordrb::Bot.new token: ENV['BOT_TOKEN']
 
 bot.message(with_text: 'Ping!') do |event|
   event.respond 'Pong!'
